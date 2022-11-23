7da2811ec08fd3d97a683440c860ebdf35083e66
Katie Hughes <katiereeshughes@gmail.com>
2022-11-23 13:48:54 -0600

    add initialization to main

--- software/apps/gripper/main.c
+++ software/apps/gripper/main.c
@@ -109,8 +109,18 @@ static float adc_sample_blocking(uint8_t channel) {
 }
 
 
+// Global variables
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 1, 0);
+
 int main(void) {
   printf("Board started!\n");
+
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  i2c_config.scl = I2C_SCL;
+  i2c_config.sda = I2C_SDA;
+  i2c_config.frequency = NRF_TWIM_FREQ_100K;
+  i2c_config.interrupt_priority = 0;
+  nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
   
   // initialize GPIO
   gpio_init();
